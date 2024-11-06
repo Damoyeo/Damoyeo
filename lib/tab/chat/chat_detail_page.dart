a0f94c3d688b010274510709886e562ec832a923
KorJIGSAW <111522731+KorJIGSAW@users.noreply.github.com>
2024-11-06 16:53:11 +0900

    Add KorJIGSAW Main Account commit
    Add main account commit

---
+++ lib/tab/chat/chat_detail_page.dart
@@ -0,0 +1,264 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:intl/intl.dart';
+
+//commit test
+class ChatDetailPage extends StatefulWidget {
+  final String chatId;
+  final String otherUserId;
+
+  const ChatDetailPage({
+    Key? key,
+    required this.chatId,
+    required this.otherUserId,
+  }) : super(key: key);
+
+  @override
+  State<ChatDetailPage> createState() => _ChatDetailPageState();
+}
+
+class _ChatDetailPageState extends State<ChatDetailPage> {
+  final TextEditingController _controller = TextEditingController();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  @override
+  void initState() {
+    super.initState();
+    markMessagesAsRead(); // 처음 진입 시 한 번 호출하여 기존 메시지 읽음 처리
+  }
+
+  // 상대방이 보낸 메시지를 읽음 상태로 변경
+  void markMessagesAsRead() async {
+    final currentUser = _auth.currentUser;
+    if (currentUser == null) return;
+
+    final unreadMessages = await _firestore
+        .collection('chats')
+        .doc(widget.chatId)
+        .collection('messages')
+        .where('senderId', isEqualTo: widget.otherUserId)
+        .where('isRead', isEqualTo: false)
+        .get();
+
+    for (var doc in unreadMessages.docs) {
+      doc.reference.update({'isRead': true});
+    }
+  }
+
+  void _sendMessage() async {
+    if (_controller.text.isNotEmpty) {
+      final currentUser = _auth.currentUser;
+      if (currentUser == null) return;
+
+      final customMessageId = "${currentUser.uid}_${DateTime.now().millisecondsSinceEpoch}";
+
+      await _firestore
+          .collection('chats')
+          .doc(widget.chatId)
+          .collection('messages')
+          .doc(customMessageId)
+          .set({
+        'messageId': customMessageId,
+        'senderId': currentUser.uid,
+        'senderName': currentUser.displayName ?? 'Unknown',
+        'message': _controller.text,
+        'timestamp': FieldValue.serverTimestamp(),
+        'isRead': false,
+      });
+
+      await _firestore.collection('chats').doc(widget.chatId).update({
+        'lastMessage': _controller.text,
+        'timestamp': FieldValue.serverTimestamp(),
+      });
+
+      _controller.clear();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        iconTheme: const IconThemeData(color: Colors.black),
+        title: Text(
+          "${widget.otherUserId}님과의 채팅방",
+          style: const TextStyle(color: Colors.black),
+        ),
+        elevation: 1,
+      ),
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          Expanded(
+            child: StreamBuilder<QuerySnapshot>(
+              stream: _firestore
+                  .collection('chats')
+                  .doc(widget.chatId)
+                  .collection('messages')
+                  .orderBy('timestamp', descending: true)
+                  .snapshots(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return const Center(child: CircularProgressIndicator());
+                }
+
+                final messages = snapshot.data!.docs;
+
+                // 새로운 메시지가 수신될 때마다 읽음 상태 업데이트
+                markMessagesAsRead();
+
+                return ListView.builder(
+                  reverse: true,
+                  itemCount: messages.length,
+                  itemBuilder: (context, index) {
+                    final messageData = messages[index];
+                    final message = messageData['message'];
+                    final senderId = messageData['senderId'];
+                    final timestamp = messageData['timestamp'] as Timestamp?;
+                    final isRead = messageData['isRead'] ?? false;
+                    final isCurrentUser = senderId == _auth.currentUser!.uid;
+
+                    // 날짜 형식으로 변환
+                    String messageDate = '';
+                    if (timestamp != null) {
+                      final date = timestamp.toDate();
+                      messageDate = DateFormat('yyyy년 M월 d일').format(date);
+                    }
+
+                    // 현재 메시지의 날짜가 이전 메시지와 다른 경우에만 날짜 표시
+                    bool showDate = false;
+                    if (index == messages.length - 1 ||
+                        DateFormat('yyyy년 M월 d일').format(
+                            (messages[index + 1]['timestamp'] as Timestamp).toDate()) !=
+                            messageDate) {
+                      showDate = true;
+                    }
+
+                    return Column(
+                      crossAxisAlignment: isCurrentUser
+                          ? CrossAxisAlignment.end
+                          : CrossAxisAlignment.start,
+                      children: [
+                        if (showDate)
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 8.0),
+                            child: Row(
+                              children: [
+                                Expanded(
+                                  child: Divider(
+                                    color: Colors.grey[400],
+                                    thickness: 0.5,
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                                  child: Text(
+                                    messageDate,
+                                    style: TextStyle(
+                                      color: Colors.grey[600],
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                ),
+                                Expanded(
+                                  child: Divider(
+                                    color: Colors.grey[400],
+                                    thickness: 0.5,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        Row(
+                          mainAxisAlignment: isCurrentUser
+                              ? MainAxisAlignment.end
+                              : MainAxisAlignment.start,
+                          children: [
+                            if (isCurrentUser && !isRead)
+                              Padding(
+                                padding: const EdgeInsets.only(right: 4.0),
+                                child: Text(
+                                  '1',
+                                  style: const TextStyle(
+                                    fontSize: 10,
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                              ),
+                            Align(
+                              alignment: isCurrentUser
+                                  ? Alignment.centerRight
+                                  : Alignment.centerLeft,
+                              child: Container(
+                                padding: const EdgeInsets.all(12),
+                                margin: const EdgeInsets.symmetric(
+                                    vertical: 5, horizontal: 10),
+                                decoration: BoxDecoration(
+                                  color: isCurrentUser
+                                      ? Colors.blue[100]
+                                      : Colors.grey[300],
+                                  borderRadius: BorderRadius.only(
+                                    topLeft: const Radius.circular(12),
+                                    topRight: const Radius.circular(12),
+                                    bottomLeft: isCurrentUser
+                                        ? const Radius.circular(12)
+                                        : const Radius.circular(0),
+                                    bottomRight: isCurrentUser
+                                        ? const Radius.circular(0)
+                                        : const Radius.circular(12),
+                                  ),
+                                ),
+                                child: Text(
+                                  message,
+                                  style: TextStyle(
+                                    color: isCurrentUser ? Colors.blue[900] : Colors.black,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _controller,
+                    decoration: InputDecoration(
+                      hintText: '메시지를 입력하세요...',
+                      filled: true,
+                      fillColor: Colors.grey[200],
+                      contentPadding: const EdgeInsets.symmetric(
+                          vertical: 10.0, horizontal: 16.0),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(24),
+                        borderSide: BorderSide.none,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 8),
+                IconButton(
+                  icon: const Icon(Icons.send, color: Colors.blue),
+                  onPressed: _sendMessage,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/tab/chat/chat_page.dart
@@ -0,0 +1,238 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'chat_detail_page.dart';
+
+class ChatPage extends StatelessWidget {
+  const ChatPage({super.key});
+
+  Future<String?> createOrGetChatRoom(String otherUserId) async {
+    final FirebaseAuth _auth = FirebaseAuth.instance;
+    final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+    final currentUser = _auth.currentUser;
+    if (currentUser == null) {
+      print("No user is logged in.");
+      return null;
+    }
+
+    final String currentUserId = currentUser.uid;
+
+    final chatQuery = await _firestore
+        .collection('chats')
+        .where('users', arrayContains: currentUserId)
+        .get();
+
+    for (var doc in chatQuery.docs) {
+      List<dynamic> users = doc['users'];
+      if (users.contains(otherUserId)) {
+        return doc.id;
+      }
+    }
+
+    String chatRoomId = _firestore.collection('chats').doc().id;
+
+    await _firestore.collection('chats').doc(chatRoomId).set({
+      'users': [currentUserId, otherUserId],
+      'lastMessage': '',
+      'timestamp': FieldValue.serverTimestamp(),
+      'pinned': false,
+    });
+
+    return chatRoomId;
+  }
+
+  Future<void> showUserListDialog(BuildContext context) async {
+    final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+    final currentUser = FirebaseAuth.instance.currentUser;
+
+    if (currentUser == null) return;
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return Dialog(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Text(
+                  "Select a user to chat with",
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+              StreamBuilder<QuerySnapshot>(
+                stream: _firestore.collection('users').snapshots(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return const Center(child: CircularProgressIndicator());
+                  }
+
+                  final users = snapshot.data!.docs.where((user) => user.id != currentUser.uid);
+
+                  return ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: users.length,
+                    itemBuilder: (context, index) {
+                      final userDoc = users.elementAt(index);
+                      final userId = userDoc.id;
+                      final userName = userDoc['name'] ?? 'Unknown';
+
+                      return ListTile(
+                        title: Text(userName),
+                        onTap: () async {
+                          Navigator.pop(context);
+                          final chatRoomId = await createOrGetChatRoom(userId);
+                          if (chatRoomId != null) {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => ChatDetailPage(
+                                  chatId: chatRoomId,
+                                  otherUserId: userId,
+                                ),
+                              ),
+                            );
+                          }
+                        },
+                      );
+                    },
+                  );
+                },
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Future<void> pinChatRoom(String chatRoomId) async {
+    final _firestore = FirebaseFirestore.instance;
+    await _firestore.collection('chats').doc(chatRoomId).update({
+      'pinned': true,
+    });
+  }
+
+  Future<void> unpinChatRoom(String chatRoomId) async {
+    final _firestore = FirebaseFirestore.instance;
+    await _firestore.collection('chats').doc(chatRoomId).update({
+      'pinned': false,
+    });
+  }
+
+  Future<void> exitChatRoom(String chatRoomId) async {
+    final _firestore = FirebaseFirestore.instance;
+    final currentUser = FirebaseAuth.instance.currentUser;
+    if (currentUser == null) return;
+
+    // 현재 사용자를 채팅방에서 제거
+    await _firestore.collection('chats').doc(chatRoomId).update({
+      'users': FieldValue.arrayRemove([currentUser.uid]),
+    });
+
+    // 채팅방에 남아있는 사용자가 없으면 채팅방 삭제
+    final updatedChatRoom = await _firestore.collection('chats').doc(chatRoomId).get();
+    if (updatedChatRoom.exists) {
+      final users = updatedChatRoom['users'] as List<dynamic>;
+      if (users.isEmpty) {
+        await _firestore.collection('chats').doc(chatRoomId).delete();
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final FirebaseAuth _auth = FirebaseAuth.instance;
+    final currentUser = _auth.currentUser;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('채팅 목록'),
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance
+            .collection('chats')
+            .where('users', arrayContains: currentUser!.uid)
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          final chats = snapshot.data!.docs;
+
+          return ListView.builder(
+            itemCount: chats.length,
+            itemBuilder: (context, index) {
+              final chat = chats[index];
+              final chatId = chat.id;
+              final lastMessage = chat['lastMessage'] ?? '';
+              final users = (chat['users'] as List).where((id) => id != currentUser.uid).toList();
+              final otherUserId = users.isNotEmpty ? users.first : null;
+              final isPinned = chat['pinned'] ?? false;
+
+              if (otherUserId == null) {
+                return const SizedBox.shrink(); // 다른 사용자가 없을 때 빈 위젯을 반환
+              }
+
+              return ListTile(
+                title: Text("Chat with $otherUserId"),
+                subtitle: Text(lastMessage),
+                trailing: isPinned ? const Icon(Icons.push_pin) : null,
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => ChatDetailPage(
+                        chatId: chatId,
+                        otherUserId: otherUserId,
+                      ),
+                    ),
+                  );
+                },
+                onLongPress: () {
+                  showModalBottomSheet(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          ListTile(
+                            leading: const Icon(Icons.push_pin),
+                            title: Text(isPinned ? '채팅방 상단 고정 해제' : '채팅방 상단 고정'),
+                            onTap: () {
+                              Navigator.pop(context);
+                              if (isPinned) {
+                                unpinChatRoom(chatId);
+                              } else {
+                                pinChatRoom(chatId);
+                              }
+                            },
+                          ),
+                          ListTile(
+                            leading: const Icon(Icons.exit_to_app),
+                            title: const Text('채팅방 나가기'),
+                            onTap: () async {
+                              Navigator.pop(context);
+                              await exitChatRoom(chatId);
+                            },
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+              );
+            },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => showUserListDialog(context),
+        child: const Icon(Icons.chat),
+      ),
+    );
+  }
+}
